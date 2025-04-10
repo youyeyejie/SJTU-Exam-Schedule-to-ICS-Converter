ee94324f2d9358a70ef076cfee856d20ce7a93ef
youyeyejie <1913220833@qq.com>
2025-04-10 13:07:30 +0800

    add config for quick login

--- exam_to_ics2.py
+++ exam_to_ics2.py
@@ -4,16 +4,24 @@ import os
 import sys
 import pytz
 import pysjtu
+import json
 
 # 获取当前执行脚本的终端路径
-running_path = os.getcwd()
+root_path = os.path.dirname(os.path.abspath(__file__))
+config_path = os.path.join(root_path, "config.json")
 
 # 登录教务系统
 if len(sys.argv) > 2:
     username = sys.argv[1]
     password = sys.argv[2]
-    print("jaccount账号：", username)
-    print("jaccount密码：", password)
+elif os.path.exists(config_path):
+    with open(config_path, "r") as f:
+        config = json.load(f)
+        username = config.get("jaccount")
+        password = config.get("password")
+    if not username or not password:
+        username = input("请输入jaccount账号：")
+        password = input("请输入jaccount密码：")
 else:
     username = input("请输入jaccount账号：")
     password = input("请输入jaccount密码：")
@@ -44,11 +52,13 @@ if not exam_schedule:
     sys.exit(1)
 else:
     print("成功获取考试安排信息。")
+    for exam in exam_schedule:
+        print(exam)
     print("正在生成ICS文件...")
 
 # 生成ICS文件保存路径
 ics_name = f"{year}-{year+1}-第{term+1}学期考试安排.ics"
-directory = os.path.join(running_path, "data")
+directory = os.path.join(root_path, "data")
 if not os.path.exists(directory):
     os.makedirs(directory)
 ics_path = os.path.join(directory, ics_name)
